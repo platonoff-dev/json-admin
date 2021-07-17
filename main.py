b4b8142768fc6e89acdcdf6d5d2856a9f66a2cb6
Anatoliy Platonov <p4m.dev@gmail.com>
2021-07-17 23:28:23 +0300

    Add possibility to view and create items

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import json
 
+import flask
+
 import schema
 import views
 
@@ -12,14 +14,12 @@ def read_schema() -> dict:
     with open("schema.json", "r") as json_file:
         return json.load(json_file)
 
-
 def main() -> None:
     app = Flask(__name__)
 
     s = schema.Schema(read_schema())
-
     app.add_url_rule("/create/", view_func=views.create_view(s))
-
+    app.add_url_rule("/list/", view_func=views.list_view(s))
     app.add_url_rule("/item/", view_func=views.create_item, methods=["POST"])
 
     app.run(port=8088)
--- views.py
+++ views.py
@@ -1,7 +1,23 @@
+from typing import List
 import flask
 from flask.templating import render_template
+import pymongo
 
 import schema
+
+
+def init_db() -> pymongo.MongoClient:
+    CONNECTION_STRING = "mongodb://admin:admin@localhost:27017/json-admin"
+
+    client = pymongo.MongoClient(CONNECTION_STRING)
+    return client
+
+
+def get_db() -> pymongo.MongoClient:
+    if 'db' not in flask.g:
+        flask.g.db = init_db()
+
+    return flask.g.db
 
 
 def create_view(s: schema.Schema):
@@ -10,7 +26,46 @@ def create_view(s: schema.Schema):
 
     return view
 
+def get_render_fields(items: List[dict]):
+    fields = set()
+    for item in items:
+        for key in item:
+            fields.add(key)
+    return fields
+
+def prepare_items(items: List[dict], render_fields: List[str]):
+    item_list = []
+    for item in items:
+        new_item = {}
+        for field in render_fields:
+            new_item[field] = item.get(field, "")
+        item_list.append(new_item)
+    return item_list
+    
+
+def list_view(s: schema.Schema):
+    def view1() -> str:
+        db: pymongo.MongoClient = get_db()
+        items_collection = db["json-admin"]["items"]
+        items = list(items_collection.find())
+
+        render_fields = s.meta.list_fields
+        if not render_fields:
+            render_fields = get_render_fields(items)
+
+        items = [x.values() for x in prepare_items(items, render_fields)]
+
+        return render_template("list_template.j2", items=items, headers=render_fields)
+
+    return view1
+
 
 def create_item():
-    print()
-    return flask.Response(status=200)
+    db: pymongo.MongoClient = get_db()
+
+    data = flask.request.form
+    
+    items_collection = db["json-admin"]["items"]
+    items_collection.insert(dict(data))
+
+    return flask.redirect("/list/")
